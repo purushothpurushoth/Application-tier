de449d415c1c365ece6b50d1d7c0ecbcf384f478
palanisamy vignesh <vpalanisamy@ntst.com>
2023-03-28 16:15:40 +0530

    spring-01

---
+++ src/main/java/com/example/newspring/NewspringApplication.java
@@ -0,0 +1,28 @@
+package com.example.newspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NewspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspringApplication.class, args);
+	}
+	@GetMapping(value = "/spring")
+	public String welcome(){
+		return "hello spring";
+	}
+    
+	@GetMapping(value = "/testing")
+	public String testing(){
+		return "working";
+	}
+     @GetMapping(value = "/sample")
+	public String sample(){
+		return "working";
+	}
+}
